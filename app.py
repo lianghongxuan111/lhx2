0ca6045f5beab792b9f200d899cedf48650c40e0
lianghongxuan111 <3521465355@qq.com>
2024-11-05 20:58:01 +0800

    lhx

--- app.py
+++ app.py
@@ -5,34 +5,41 @@ app = Flask(__name__)
 
 
 def create_table():
-    # 连接到MySQL数据库
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="123456",
-        database="python"
-    )
-    mycursor = mydb.cursor()
-    # 创建用户表，如果不存在
-    mycursor.execute('''CREATE TABLE IF NOT EXISTS users
-                      (username VARCHAR(255))''')
-    mydb.commit()
-    mydb.close()
+    try:
+        # 连接到MySQL数据库
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="123456",
+            database="python"
+        )
+        mycursor = mydb.cursor()
+        # 创建用户表，如果不存在
+        mycursor.execute('''CREATE TABLE IF NOT EXISTS users
+                          (username VARCHAR(255))''')
+        mydb.commit()
+        mydb.close()
+    except mysql.connector.Error as err:
+        print(f"创建表时出错: {err}")
 
 
 def login(username):
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="123456",
-        database="python"
-    )
-    mycursor = mydb.cursor()
-    # 查询用户是否存在
-    mycursor.execute("SELECT * FROM users WHERE username = %s", (username,))
-    user = mycursor.fetchone()
-    mydb.close()
-    return user is not None
+    try:
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="123456",
+            database="python"
+        )
+        mycursor = mydb.cursor()
+        # 查询用户是否存在
+        mycursor.execute("SELECT * FROM users WHERE username = %s", (username,))
+        user = mycursor.fetchone()
+        mydb.close()
+        return user is not None
+    except mysql.connector.Error as err:
+        print(f"登录验证时出错: {err}")
+        return False
 
 
 @app.route('/a', methods=['GET', 'POST'])
@@ -45,17 +52,21 @@ def page_a():
             return redirect('/b?username=' + username)
         else:
             # 注册新用户
-            mydb = mysql.connector.connect(
-                host="localhost",
-                user="root",
-                password="123456",
-                database="python"
-            )
-            mycursor = mydb.cursor()
-            mycursor.execute("INSERT INTO users (username) VALUES (%s)", (username,))
-            mydb.commit()
-            mydb.close()
-            return redirect('/b?username=' + username + '&is_new_user=1')
+            try:
+                mydb = mysql.connector.connect(
+                    host="localhost",
+                    user="root",
+                    password="123456",
+                    database="python"
+                )
+                mycursor = mydb.cursor()
+                mycursor.execute("INSERT INTO users (username) VALUES (%s)", (username,))
+                mydb.commit()
+                mydb.close()
+                return redirect('/b?username=' + username + '&is_new_user=1')
+            except mysql.connector.Error as err:
+                print(f"注册新用户时出错: {err}")
+                return "注册失败，请稍后重试"
     return render_template('a.html')
 
 
@@ -66,8 +77,9 @@ def page_b():
     if is_new_user:
         welcome_message = "欢迎新用户 " + username
     else:
+
         welcome_message = "欢迎用户 " + username
-    return render_template('b.html', username = username, welcome_message = welcome_message)
+    return render_template('b.html', username=username, welcome_message=welcome_message)
 
 
 if __name__ == '__main__':
